a17f020cf1240ffbf9cebbf453478ef30eb1c249
Emre Kultursay <emrekultursay@gmail.com>
2024-10-11 06:11:50 -0700

    Build and run Sherlock from inside IntelliJ IDEA (#7)
    * Use IC as the platform type code
    
    Platform Type is a code that determines which IDE the platform represents (e.g., IDEA, Android Studio, PyCharm, etc).  The code 'MP' is not recognized by the IntelliJ Platform Gradle Plugin, so it gives errors when building the plugin against our custom platform. 
    
    The code for IntellliJ IDEA is `IC`, so let's use that one.
    
    * Build and run Sherlock from inside IntelliJ IDEA
    
    This change:
    
    1. Adds an `.iml` file that represents the Sherlock platform.
       The `.iml` contains the absolute minimum for the Sherlock platform.
       The contents should be kept in sync (or mostly similar) with the
       list of modules used in the `build_sherlock_platform.sh` script.
    2. Adds a run configuration that is identical to the `IDEA` run
       configuration except that (1) it uses the Sherlock `.iml` file for
       the classpath, (2) uses SherlockPlatform as the platorm prefix.
    3. Updates getPlugins.sh to download the android repo at the correct
       tag automatically.
    
    Using this change, one can:
    
    1. Clone android-graphics/intellij-community
    2. Run ./getPlugins.sh --shallow
    3. Open the project in IntelliJ IDEA
    4. Switch to Sherlock run configuration
    5. Hit Run/Debug to build and launch Sherlock

--- getPlugins.sh
+++ getPlugins.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+# This script clones the Jetbrains/android repository. Note that the tag
+# of the android repo must match the tag of the IntelliJ repo, otherwise
+# there can be build errors.
+
+# This value does not contain the actual snapshot value.
+readonly AS_BUILD_NUMBER="$(sed 's/\.SNAPSHOT$//' build.txt)"
+
+# When we pull a new IntelliJ platform, we must update this SNAPSHOT value.
+# This can be automated (e.g., search all git tags for this prefix), but
+# the benefit doesn't seem worth the time spent on automating it.
+readonly SNAPSHOT="142"
+
+readonly TAG="idea/${AS_BUILD_NUMBER}.${SNAPSHOT}"
+
+echo "Cloning Jetbrains/android repository using the following tag: ${TAG}"
+
 if [ "$1" == "--shallow" ]; then
-    git clone git://git.jetbrains.org/idea/android.git android --depth 1
+    git clone git://git.jetbrains.org/idea/android.git android --depth 1 --branch "${TAG}"
 else
-    git clone git://git.jetbrains.org/idea/android.git android
+    echo "Warning: Cloning with the entire history. Use the --shallow flag to clone faster."
+    git clone git://git.jetbrains.org/idea/android.git android --branch "${TAG}"
 fi
