985056f6c399629960cb162f268d2611822c977a
Emre Kultursay <emrekultursay@gmail.com>
2024-11-01 07:57:45 -0700

    Don't build for mac-x64 platform (#11)
    On MacOS, we only need to support arm64. No need to
    build the x64 version.

--- build_sherlock_platform.sh
+++ build_sherlock_platform.sh
@@ -12,7 +12,7 @@ BUILD_PROPERTIES=(
   "-Dbuild.number=${AS_BUILD_NUMBER}"
   "-Dintellij.build.dev.mode=false"
   "-Dcompile.parallel=true"
-  "-Dintellij.build.skip.build.steps=repair_utility_bundle_step,mac_dmg,mac_sign,mac_sit,windows_exe_installer,linux aarch64,windows aarch64" # TODO
+  "-Dintellij.build.skip.build.steps=repair_utility_bundle_step,mac_dmg,mac_sign,mac_sit,windows_exe_installer,linux aarch64,windows aarch64,mac x64" # TODO
   "-Dintellij.build.incremental.compilation=true" # TODO
   "-Dintellij.build.incremental.compilation.fallback.rebuild=false"
 )
