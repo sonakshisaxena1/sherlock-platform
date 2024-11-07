06e75d5b7e5a2c662efce34eecb30f81713e75d8
Sonakshi Saxena <nexa@google.com>
2024-11-07 13:57:52 +0000

    Streamline Sherlock Platform build by integrating plugin fetching from the build script (#19)
    * Setup GitHub actions to build IntelliJ Platform.
    
    * Setup GitHub actions to build IntelliJ Platform.
    
    * Streamline Sherlock Platform build by integrating plugin fetching from the build script

--- build_sherlock_platform.sh
+++ build_sherlock_platform.sh
@@ -12,9 +12,23 @@ BUILD_PROPERTIES=(
   "-Dbuild.number=${AS_BUILD_NUMBER}"
   "-Dintellij.build.dev.mode=false"
   "-Dcompile.parallel=true"
-  "-Dintellij.build.skip.build.steps=repair_utility_bundle_step,mac_dmg,mac_sign,mac_sit,windows_exe_installer,linux aarch64,windows aarch64,mac x64" # TODO
-  "-Dintellij.build.incremental.compilation=true" # TODO
+  "-Dintellij.build.skip.build.steps=repair_utility_bundle_step,mac_dmg,mac_sign,mac_sit,windows_exe_installer,linux aarch64,windows aarch64,mac x64"
+  "-Dintellij.build.incremental.compilation=true"
   "-Dintellij.build.incremental.compilation.fallback.rebuild=false"
 )
+
+# Use --with-android to update/download android repo before building
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --with-android)
+      ./getPlugins.sh --shallow
+      shift
+      ;;
+    *)
+      echo "Unknown option: $1" >&2
+      exit 1
+      ;;
+  esac
+done
 
 "${PROG_DIR}/platform/jps-bootstrap/jps-bootstrap.sh" "${BUILD_PROPERTIES[@]}" "${PROG_DIR}" intellij.idea.community.build SherlockPlatformBuild
